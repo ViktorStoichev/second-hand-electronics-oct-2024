f3ec9c8b9fbc2b005f2f1c96f6b3a538097683b5
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-10-17 09:23:43 +0300

    Setup cookie-parser and jsonwebtoken

--- src/config/expressInit.js
+++ src/config/expressInit.js
@@ -1,7 +1,9 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
 
 export default function expressInit(app) {
     app.use('/css', express.static('src/css'));
     app.use('/image', express.static('src/image'));
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 }
