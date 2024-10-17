da70dd74b6434c9db62405588d230c5db0605720
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-10-17 08:49:59 +0300

    Add database with mongoose and add user model

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import routes from './routes.js';
 import expressInit from './config/expressInit.js';
 import handlebarsInit from './config/handlebarsInit.js';
+import mongooseInit from './config/mongooseInit.js';
 
 const app = express();
 const port = 3000;
 
+mongooseInit();
 handlebarsInit(app);
 expressInit(app);
 
